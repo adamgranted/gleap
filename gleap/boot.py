d608c716a8cb8d3282d743d7067952af78b2bf2a
Adam Grant <adamgranted@users.noreply.github.com>
2024-10-15 17:44:45 -0700

    Update boot.py

--- gleap/boot.py
+++ gleap/boot.py
@@ -9,17 +9,17 @@ def boot_session(bootinfo):
             # Try to get the single instance of Gleap Settings
             gleap_settings = frappe.get_single("Gleap Settings")
             if gleap_settings and gleap_settings.enable_gleap:
-                bootinfo.gleap_api_key = gleap_settings.gleap_api_key
-                bootinfo.enable_gleap = gleap_settings.enable_gleap
+                bootinfo['gleap_api_key'] = gleap_settings.gleap_api_key
+                bootinfo['enable_gleap'] = gleap_settings.enable_gleap
             else:
-                bootinfo.gleap_api_key = None
-                bootinfo.enable_gleap = False
+                bootinfo['gleap_api_key'] = None
+                bootinfo['enable_gleap'] = False
         else:
             # 'Gleap Settings' DocType does not exist yet
-            bootinfo.gleap_api_key = None
-            bootinfo.enable_gleap = False
+            bootinfo['gleap_api_key'] = None
+            bootinfo['enable_gleap'] = False
     except Exception as e:
         # Log the exception without interrupting the boot process
         frappe.log_error(message=frappe.get_traceback(), title="Gleap Boot Session Error")
-        bootinfo.gleap_api_key = None
-        bootinfo.enable_gleap = False
+        bootinfo['gleap_api_key'] = None
+        bootinfo['enable_gleap'] = False
